d0acbfb47c7cfba3d58d129a436d3d3568d8e692
ech con dam dang <haunghia1512@gmail.com>
2025-11-16 09:23:08 +0700

    Delete lab02 directory

--- lab02/lab4_9.php
+++
@@ -1,3 +0,0 @@
-<?php
-    echo"diem khac biet cua include va require la vs include neu file truy cap khong ton hay hoac sai duogn dan thi se khong ngat code va tiep tuc xu ly cac dong ma ben duoi doi voi require thi nguoc lai se dung toan bo ma xu ly ben duoi va khong chay nua";
-?>
--- lab02/lab5_1.php
+++
@@ -1,6 +0,0 @@
-<?php
-    $a=3;
-    $b=2;
-    echo "so nguyen ".$nguyen =intdiv($a,$b);
-    echo "so du". $du=$a%$b;
-?>
